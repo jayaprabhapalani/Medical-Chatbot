d3ebe1886525528aa83f6572d168d9f54d3fc553
jayaprabhapalani <146448216+jayaprabhapalani@users.noreply.github.com>
2025-11-26 21:53:42 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Jayaprabha",
+    author_email="yazhh1008@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
